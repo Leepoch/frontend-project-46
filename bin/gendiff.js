8c10298b98f50663c1ce252f63161f85c50735de
Daniil Tinkov <daniiltinkovv@gmail.com>
2022-09-04 15:25:16 +0300

    update step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,4 +6,6 @@ program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format <type>', 'output format')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
     .parse()
