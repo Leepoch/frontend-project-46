ea07ee25b748e54a4300eb3e17c6ace2643dfbc7
Daniil Tinkov <daniiltinkovv@gmail.com>
2022-09-05 17:57:30 +0300

    step 3 for verification

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,14 +2,30 @@
 
 import { program } from 'commander';
 import { readFileSync } from 'node:fs';
+import _ from 'lodash';
 
 const genDiff = (filepath1, filepath2) => {
-    const jsonData = readFileSync('file1.json');
-    const parseData = JSON.parse(jsonData); 
-    return parseData;
-}
-    
+    const jsonData1 = readFileSync(filepath1, 'utf-8');
+    const jsonData2 = readFileSync(filepath2, 'utf-8');
+    const data1 = JSON.parse(jsonData1);
+    const data2 = JSON.parse(jsonData2);
+    const result = {};
+    const entries = Object.entries(data1)
 
+    for (let [key, value] of entries) {
+        if (_.has(data2, key) && data2[key] !== data1[key]) {
+            result[`- ${key}`] = data1[key];
+            result[`+ ${key}`] = data2[key];
+        } else if (!_.has(data1, key)) {
+            result[`+ ${key}`] = value
+        } else if (_.has(data2, key) && data2[key] === data1[key]) {
+            result[key] = value;
+        } else {
+            result[`- ${key}`] = data1[key];
+        };
+    };
+    console.log(result);
+}
 program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
@@ -19,5 +35,4 @@ program
     .action((filepath1, filepath2) => {
         console.log(genDiff(filepath1, filepath2));
     })
-    .parse()
-
+program.parse();
