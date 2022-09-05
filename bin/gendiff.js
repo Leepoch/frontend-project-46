6a9ea67880e3534355f7a76119c301b1947b68bc
Daniil Tinkov <daniiltinkovv@gmail.com>
2022-09-05 13:17:52 +0300

    step 3 for verification

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 
-import { program } from 'commander'
+import { program } from 'commander';
+import { readFileSync } from 'node:fs';
+
+const genDiff = (filepath1, filepath2) => {
+    const jsonData = readFileSync('file1.json');
+    const parseData = JSON.parse(jsonData); 
+    return parseData;
+}
+    
 
 program
     .version('0.0.1')
@@ -8,4 +16,8 @@ program
     .option('-f, --format <type>', 'output format')
     .argument('<filepath1>')
     .argument('<filepath2>')
+    .action((filepath1, filepath2) => {
+        console.log(genDiff(filepath1, filepath2));
+    })
     .parse()
+
