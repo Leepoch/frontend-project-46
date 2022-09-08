eba381c9795ffacc85e9034e919dcfdfd66d5b48
Daniil Tinkov <daniiltinkovv@gmail.com>
2022-09-08 15:36:14 +0300

    step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,22 +9,25 @@ const genDiff = (filepath1, filepath2) => {
     const jsonData2 = readFileSync(filepath2, 'utf-8');
     const data1 = JSON.parse(jsonData1);
     const data2 = JSON.parse(jsonData2);
-    const result = {};
-    const entries = Object.entries(data1)
+    const keysData1 = _.keys(data1);
+    const keysData2 = _.keys(data2);
+    const entries = _.union(keysData1, keysData2);
+    entries.sort();
 
-    for (let [key, value] of entries) {
-        if (_.has(data2, key) && data2[key] !== data1[key]) {
-            result[`- ${key}`] = data1[key];
-            result[`+ ${key}`] = data2[key];
-        } else if (!_.has(data1, key)) {
-            result[`+ ${key}`] = value
-        } else if (_.has(data2, key) && data2[key] === data1[key]) {
-            result[key] = value;
-        } else {
-            result[`- ${key}`] = data1[key];
-        };
-    };
-    console.log(result);
+    const result = entries.map((key) => {
+      if (!_.has(data1, key)) {
+        return `+ ${key}: ${data1[key]}`
+      } else if (!_.has(data2, key)) {
+        return `- ${key}: ${data1[key]}`
+      } else if (_.has(data1, key) && _.has(data2, key) && data1[key] === data2[key]) {
+        return `${key}: ${data1[key]}`
+      } else if (data1[key] !== data2[key]) {
+        return `- ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`
+      } else {
+        return `${key}: ${data1[key]}`
+      }
+    })
+    console.log(`{\n${result.join('\n')}\n}`)
 }
 program
     .version('0.0.1')
